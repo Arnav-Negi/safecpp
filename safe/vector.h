223476ef1ed3a1e910c344068f19885d29ef252b
Arnav-Negi <arnav.negi@students.iiit.ac.in>
2024-12-07 00:45:00 +0530

    ignore

--- safe/vector.h
+++ safe/vector.h
@@ -5,6 +5,21 @@
 #ifndef VECTOR_H
 #define VECTOR_H
 
-namespace
+#include <vector>
+
+namespace safe
+{
+    /// vector: imitates std::vector except it always using the throwing at method
+    /// instead of [] operator
+    /// @tparam T The underlying type
+    /// Has most of the methods for std::vector, but can be extended
+    template <typename T>
+    class vector
+    {
+        std::vector<T> base;
+    public:
+
+    };
+}
 
 #endif //VECTOR_H
